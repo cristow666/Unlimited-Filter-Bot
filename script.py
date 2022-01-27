d932a581096889e767d2bcbfe3217f1356002f37
cristow666 <85900542+cristow666@users.noreply.github.com>
2022-01-27 09:55:33 +0530

    Update script.py

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ See <i>/help</i> for commands and more details.</b>
 
 
     HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
+<i>You Can See Me At @Televisionmarket :)</i>
 
 
 <b>Basic Commands;</b>
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @Televisionmarket</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Tʜᴏᴍᴀs Sʜᴇʟʙʏ</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @cristow666    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
